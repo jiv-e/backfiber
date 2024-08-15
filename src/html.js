fe903f2862323a78aa94c93bae435d6a28627fc3
Juho Viitasalo <juho.viitasalo@lildrop.com>
2024-08-15 23:16:18 +0300

    Change client folder name to vendor

--- src/html.js
+++ src/html.js
@@ -12,7 +12,11 @@ export function html(
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>${props.meta.title}</title>
+      <title>${state.meta.title}</title>
+      <meta name="description" content="${state.meta.description}">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap" rel="stylesheet">
       ${styles}
       ${js}
     </head>
